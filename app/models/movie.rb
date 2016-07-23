d30004a02c95acbbed77bac5ecb970d192e88dc5
apoorv1704 <apoorvshrivastava1704@gmail.com>
2016-07-23 15:11:59 +0530

    solved commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,9 +5,6 @@ class Movie < ActiveRecord::Base
 
 
 	has_attached_file :image, styles: { medium: "400x600#" }
-	:storage => :s3,
-    :bucket => " fanatics ",
-    :default_url => '/images/:attachment/missing_:style.png',
-    :s3_credentials => S3_CREDENTIALS
+	
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
