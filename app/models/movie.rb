58aa2f4cdfee1b01127c27bbf62aebc13e911e59
apoorv1704 <apoorvshrivastava1704@gmail.com>
2016-07-23 15:04:05 +0530

    Conflict resolved

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,5 +5,9 @@ class Movie < ActiveRecord::Base
 
 
 	has_attached_file :image, styles: { medium: "400x600#" }
+	:storage => :s3,
+    :bucket => " fanatics ",
+    :default_url => '/images/:attachment/missing_:style.png',
+    :s3_credentials => S3_CREDENTIALS
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
