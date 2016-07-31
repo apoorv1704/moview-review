f73df569549ac1cedc43191cfe2e985187474b17
apoorv1704 <apoorvshrivastava1704@gmail.com>
2016-07-31 22:42:20 +0530

    new server

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -48,7 +48,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to reviews_url, notice: 'Review was successfully DELETED.' }
       format.json { head :no_content }
     end
   end
