0718d704e8eaa88b236d580863234b255c7c237a
apoorv1704 <apoorvshrivastava1704@gmail.com>
2016-08-07 14:10:25 +0530

    Working on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :movie
-  has_many :reviews, dependent: :destroy
+  has_many :review, dependent: :destroy
 end
