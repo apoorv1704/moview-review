5b5f2f37a1df916f0fe80f6bf7f0d8b106b9a158
apoorv1704 <apoorvshrivastava1704@gmail.com>
2016-08-18 00:07:52 +0530

    resolving

--- app/assets/javascripts/jquery.raty.js
+++ app/assets/javascripts/jquery.raty.js
@@ -726,7 +726,7 @@
     noRatedMsg   : 'Not rated yet!',
     number       : 5,
     numberMax    : 20,
-    path         : '/assets/images/',
+    path         : '/assets/images',
     precision    : false,
     readOnly     : false,
     round        : { down: 0.25, full: 0.6, up: 0.76 },
